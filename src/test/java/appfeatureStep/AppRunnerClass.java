8d39d90f259d5937166742d470b4e4dd4c099ecf
AviWagh <93674345+AviWagh@users.noreply.github.com>
2024-03-28 07:33:51 +0530

    code push

--- src/test/java/appfeatureStep/AppRunnerClass.java
+++ src/test/java/appfeatureStep/AppRunnerClass.java
@@ -6,14 +6,18 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions
 (
 		features={"src\\test\\resources\\AppFeature"},
+		//features={"@target/failedTest.txt"},
 		glue={"appfeatureStep"},
-		plugin={"pretty","html:target/cucumber-reports/amazonreports.html"},
+		plugin={"pretty","html:target/cucumber-reports/amazonreports.html","rerun:target/cucumber-reports/failedTest.txt"},
+		//plugin={"pretty","rerun:target/cucumber-reports/failedTest.txt"},
+
 		tags="",
+		publish=true,
 		dryRun=false
-)
+		)
 
 public class AppRunnerClass extends AbstractTestNGCucumberTests 
 {
-	
+
 
 }
